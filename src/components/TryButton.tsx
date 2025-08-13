2389f16807cf6d9be77aa71574127aad43042adf
samarthmishra46 <119853181+samarthmishra46@users.noreply.github.com>
2025-08-13 09:42:40 +0530

    Update TryButton.tsx

--- src/components/TryButton.tsx
+++ src/components/TryButton.tsx
@@ -9,7 +9,7 @@ export function TryButton(){
                       className="relative inline-flex items-center px-6 py-3 bg-[#9F6AEA] text-white font-semibold rounded-lg text-sm sm:text-base md:text-lg"
                     >
                       <span className="flex items-center relative z-10 pointer-events-auto">
-                        Try For 30 Days At Just Rs.1999
+                        Try For 30 Days At Just Rs.1749
                         <ChevronRight className="ml-2 w-4 h-4 sm:w-5 sm:h-5 group-hover:translate-x-1 transition-transform" />
                       </span>
                     </Link>
